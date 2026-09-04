a760aaadcac62156f48ab2a44b7bd3bfd1b417ee
vasuarnav94-afk <vasuarnav94@gmail.com>
2026-09-04 19:10:44 +0530

    Delete lec15-auth-frontend directory

--- lec15-auth-frontend/src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import authReducer from "../features/AuthSlice"
-
-export const store = configureStore({
-    reducer:{
-        auth: authReducer,
-    }
-});
