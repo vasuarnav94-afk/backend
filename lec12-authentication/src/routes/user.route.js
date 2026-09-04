ffdf2f4911caf960785641ac84d465bafb3fc6f0
vasuarnav94-afk <vasuarnav94@gmail.com>
2026-09-04 19:10:18 +0530

    Delete lec12-authentication directory

--- lec12-authentication/src/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const registerController = require("../controller/user.controller");
-
-const router = express.Router();
-
-router.post("/register", registerController)
-
-module.exports = router
